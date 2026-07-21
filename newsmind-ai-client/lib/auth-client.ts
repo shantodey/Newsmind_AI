04b6d32417a065945bfa45deb19b67d0569210a9
shantodey <ratul3330@gmail.com>
2026-07-22 01:58:53 +0600

    refactor: enhance authClient configuration and improve session management in auth module

--- newsmind-ai-client/lib/auth-client.ts
+++ newsmind-ai-client/lib/auth-client.ts
@@ -1,7 +1,19 @@
-import { createAuthClient } from "better-auth/react";
 
+
+import { jwtClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
+  plugins: [jwtClient()],
 });
 
-export const { signIn, signUp, signOut, useSession, getSession } = authClient;
+// google
+export const googleSignIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google",
+  });
+  return data;
+};
+
+
+export const { signIn, signUp, useSession } = createAuthClient();
--- newsmind-ai-client/lib/auth.ts
+++ newsmind-ai-client/lib/auth.ts
@@ -1,29 +1,45 @@
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
-const client = new MongoClient(process.env.MONGODB_URI!);
+const client = new MongoClient(process.env.MONGODB_URI as string);
 const db = client.db("newsmind-ai");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, { client }),
-  secret: process.env.BETTER_AUTH_SECRET!,
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  database: mongodbAdapter(db, {
+    client,
+  }),
+
   emailAndPassword: {
     enabled: true,
-    requireEmailVerification: false,
   },
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+
   session: {
-    expiresIn: 60 * 60 * 24 * 7,
-    updateAge: 60 * 60 * 24,
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5,
+    },
+    strategy: "jwt",
   },
-  user: {
+
+  plugins: [
+    jwt({
+      jwt: {
+        expirationTime: "15m",
+      },
+    }),
+  ],
+
+    user: {
     additionalFields: {
       role: {
         type: "string",
